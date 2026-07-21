c730a6273ef54573cab5ede8a18d08ac5fbf9eb2
John Doe <johndoe@example.com>
2026-07-21 19:52:18 +0300

    commit 1

---
+++ 1.py
@@ -0,0 +1,25 @@
+from collections import deque
+import sys
+
+dq = deque([1, 2, 3])
+# dq.append(4) # добавить справа
+# dq.appendleft(0) # добавить слева
+# print(dq)
+# dq.pop() # удалить справа
+# dq.popleft() # удалить слева
+# print(dq)
+print(sys.getsizeof(dq))
+
+# вставка в начало → O(1)
+# вставка в конец → O(1)
+# удаление с начала → O(1)
+# удаление с конца → O(1)
+
+# очереди
+# queue = deque()
+# queue.append("A")
+# queue.append("B")
+# print(queue.popleft()) # A
+# print(queue)
+
+# стеки
---
+++ 2.py
@@ -0,0 +1,13 @@
+import sys
+set = {4,3,1,8,2,5}
+# print(set)
+# print(hash(1))
+# set.add(7)
+# set.add(7)
+# print(set)
+# set.add(100)
+# print(set)
+
+l = (4,3,1,8,2,5)
+print(sys.getsizeof(set))
+print(sys.getsizeof(l))
